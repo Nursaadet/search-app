7713dcf07e0780bc1772c0aecb12989905d1678f
Nursaadet <nursaadetozdemir@gmail.com>
2025-05-28 17:14:19 +0300

    Add functions to display and clear searched images

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const searchInput = document.querySelector("#searchInput");
 const buttonWrapper = document.querySelector(".button-wrapper");
 const searchButtton = document.querySelector("#searchButton");
 const clearButton = document.querySelector("#clearButton");
-const imageListWrapper = document.querySelector(".imageListWrapper");
+const imageListWrapper = document.querySelector(".imageList-wrapper");
 
 runEventListeners();
 function runEventListeners() {
   form.addEventListener("submit", search);
+  clearButton.addEventListener("click", clear)
+}
+
+function clear(){
+    searchInput.value="";
+    Array.from(imageListWrapper.children).forEach((child)=> child.remove())
 }
 
 function search(e) {
@@ -23,9 +29,25 @@ function search(e) {
     .then((data) => {
       Array.from(data.results).forEach((image) => {
         console.log(image.urls.small);
+        addImageToUI(image.urls.small)
       });
     })
     .catch((err) => console.log(err));
 
   e.preventDefault();
+}
+
+function addImageToUI(url){ 
+
+    console.log(imageListWrapper)
+    const div= document.createElement("div");
+    div.className="card";
+
+    const img= document.createElement("img");
+    img.setAttribute("src",url);
+    img.height='400';
+    img.width='400';
+
+    div.append(img);
+    imageListWrapper.append(div);
 }
